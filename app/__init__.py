de1f42cc14d6618c5227cfaeb6a40b6a53260e0d
Mykola Podopryhor <ruberoidzz@gmail.com>
2023-09-13 22:08:25 +0200

    Login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from config import Config
+from flask_bootstrap import Bootstrap5
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
+app.config.from_object(Config)
+# Bootstrap-Flask requires this line
+bootstrap = Bootstrap5(app)
+# Flask-WTF requires this line
+csrf = CSRFProtect(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -34,3 +35,14 @@ def index():
     ]	
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user, books=books)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
